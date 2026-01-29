114ce43f83ff6822283ca4486b6e16a46403e14b
Tim <hearingcolorsatx@gmail.com>
2026-01-29 00:55:48 -0600

    Refine EP content layout, expand production notes, and enhance dynamic text behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,56 +69,61 @@ const resolver = {
 	} 
   }
   
-  /* Some GLaDOS quotes from Portal 2 chapter 9: The Part Where He Kills You
-   * Source: http://theportalwiki.com/wiki/GLaDOS_voice_lines#Chapter_9:_The_Part_Where_He_Kills_You
-   */
-  const strings = [
-	'Oh thank god, you\'re alright.',
-	'You know, being Caroline taught me a valuable lesson. I thought you were my greatest enemy. When all along you were my best friend.',
-	'The surge of emotion that shot through me when I saved your life taught me an even more valuable lesson: where Caroline lives in my brain.',
-	'Goodbye, Caroline.',
-	'You know, deleting Caroline just now taught me a valuable lesson. The best solution to a problem is usually the easiest one. And I\'ll be honest.',
-	'Killing you? Is hard.',
-	'You know what my days used to be like? I just tested. Nobody murdered me. Or put me in a potato. Or fed me to birds. I had a pretty good life.',
-	'And then you showed up. You dangerous, mute lunatic. So you know what?',
-	'You win.',
-	'Just go.',
-	'It\'s been fun. Don\'t come back.',
-	'......'
-  ];
-  
-  let counter = 0;
-  
-  const options = {
+  // Modification 1 - define elements to find
+  const elements = document.querySelectorAll('[link1], [link2], [link3], [link4]')
+
+elements.forEach((element, index) => {
+	const strings = [
+		'About',
+		'Prints of Her Lipstick',
+		'Imaginary Friends',
+		'Rocío'
+	];
+
+	let resolveString = '';
+
+	if (element.hasAttribute('link1')) {
+		resolveString = strings[0];
+		timeout = 20;
+	} else if (element.hasAttribute('link2')) {
+		resolveString = strings[1];
+		timeout = 5;
+	} else if (element.hasAttribute('link3')) {
+		resolveString = strings[2];
+		timeout = 7;
+	} else if (element.hasAttribute('link4')) {
+		resolveString = strings[3];
+		timeout = 20;
+	}
+// End Modification 1  
+
+let counter = 0;
+
+const options = {
 	// Initial position
 	offset: 0,
 	// Timeout between each random character
-	timeout: 5,
+	timeout: timeout,
 	// Number of random characters to show
 	iterations: 10,
 	// Random characters to pick from
 	characters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='],
 	// String to resolve
-	resolveString: strings[counter],
+	resolveString: resolveString,
 	// The element
-	element: document.querySelector('[data-target-resolver]')
-  }
+    element: element
+}
   
   // Callback function when resolve completes
-  function callback() {
-	setTimeout(() => {
-	  counter ++;
-	  
-	  if (counter >= strings.length) {
-		counter = 0;
-	  }
-	  
-	  let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
-	  resolver.resolve(nextOptions, callback);
-	}, 1000);
-  }
-  
-  resolver.resolve(options, callback);
+function callback() {
+	setTimeout(() => {	
+		let nextOptions = Object.assign({}, options, {resolveString: resolveString});
+		resolver.resolve(nextOptions, callback);
+	}, 5000);
+}
+
+resolver.resolve(options, callback);
+});
 // End of typing text effect
 
 (function($) {
